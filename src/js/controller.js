d90983285700c72a5b9ddfd68bb992d7b1838847
Stanislau Marudau <marudov.stas@gmail.com>
2023-12-29 21:05:27 +0200

    Started data fetching.

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,6 +8,29 @@ const timeout = function (s) {
   });
 };
 
+
+const showRecipy = async function () {
+  try {
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcb37');
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`)
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    }
+
+  } catch (err) { alert(err.message) }
+}
+
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
